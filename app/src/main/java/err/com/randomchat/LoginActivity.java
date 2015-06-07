9dbb259211b2e007f1e0b81764a94d64d831934b
dudhead <karthik.k@hashedin.com>
2015-06-08 00:05:24 +0530

    working model of GCM client implementation.

--- app/src/main/java/err/com/randomchat/LoginActivity.java
+++ app/src/main/java/err/com/randomchat/LoginActivity.java
@@ -2,16 +2,24 @@ package err.com.randomchat;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -22,25 +30,53 @@ import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
 
+import err.com.randomchat.gcm.RegistrationIntentService;
+
 
 public class LoginActivity extends Activity {
+
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "MainActivity";
 
 
     //facebook callbacks manager
     private CallbackManager cm;
     private LoginButton mFbLoginButton;
 
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private TextView mInformationTextView;
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         FacebookSdk.sdkInitialize(this.getApplicationContext());
 
-        try {
+
+       /* try {
             PackageInfo info = getPackageManager().getPackageInfo(
                     "err.com.randomchat",
                     PackageManager.GET_SIGNATURES);
@@ -54,12 +90,32 @@ public class LoginActivity extends Activity {
 
         } catch (NoSuchAlgorithmException e) {
 
-        }
+        }*/
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //init facebook sdk and
+        mInformationTextView = (TextView) findViewById(R.id.username);
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+                public void onReceive(Context context, Intent intent) {
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER.name(), false);
+                if (sentToken) {
+                    mInformationTextView.setText(getString(R.string.gcm_send_message));
+                } else {
+                    mInformationTextView.setText(getString(R.string.token_error_message));
+                }
+            }
+        };
 
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
 
         //instantiate callbacks manager
         cm = CallbackManager.Factory.create();
@@ -138,5 +194,18 @@ public class LoginActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         cm.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE.name()));
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
     }
 }
---
+++ app/src/main/java/err/com/randomchat/QuickstartPreferences.java
@@ -0,0 +1,8 @@
+package err.com.randomchat;
+
+/**
+ * Created by hasher on 7/6/15.
+ */
+public enum QuickstartPreferences {
+    SENT_TOKEN_TO_SERVER,REGISTRATION_COMPLETE
+}
